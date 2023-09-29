a19a0f7c41332e3de25af881898080fabbd074c8
tan-sahil <tha.sahu@gmail.com>
2023-09-29 14:29:11 +0530

    added packages and integrated server with nodejs

---
+++ app.js
@@ -0,0 +1,17 @@
+const app  = require('express')();
+const {createServer} = require('node:http');
+const {Server} = require('socket.io');
+
+const server = createServer(app);
+
+const io = new Server(server);
+
+app.get("/", (req, res) =>{
+    res.send("<h1>Chat Application</h1>")
+})
+
+io.on('connection', (socket) =>{
+    console.log(`connected to ${socket}`)
+})
+
+app.listen(3000, () => console.log("listning on the port 3000 ......"));
