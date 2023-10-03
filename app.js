1f60506fcafb3c68a1f90929fccea25349edd894
tan-sahil <tha.sahu@gmail.com>
2023-10-03 11:46:03 +0530

    added socket on server and client side

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const app  = require('express')();
+const express = require('express');
 const {createServer} = require('node:http');
 const {Server} = require('socket.io');
+app.set("view engine", 'ejs');
 
 const server = createServer(app);
 
 const io = new Server(server);
 app.use(express.json())
 app.get("/", (req, res) =>{
-    res.send("<h1>Chat Application</h1>")
+    res.render('dummy')
 })
 
 io.on('connection', (socket) =>{
-    console.log(`connected to ${socket}`)
-})
 
-app.listen(3000, () => console.log("listning on the port 3000 ......"));
+
+
+//    socket.broadcast.emit('hello')
+    
+
+     socket.on('chat message', (msg) =>{
+        io.emit('chat message', msg);
+     })    
+  
+    // socket.on('disconnect', () =>{
+    //     console.log(`user is disconnected`);
+    // })
+
+})
+server.listen(3000, () => console.log("listning on the port 3000 ......"));
